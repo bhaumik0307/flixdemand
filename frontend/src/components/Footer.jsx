4104c55e5ed56261386d3f4162b8fd14914053b7
Bhaumik Chauhan <127184237+bhaumik0307@users.noreply.github.com>
2024-07-07 21:43:12 +0530

    target blank

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
                 </div>
 
                 <ul className="flex flex-row items-center">
-                    <li className="mx-2"><a className="" href="https://www.instagram.com/bhaumik_chauhan_08/"><FaInstagram size={24}/></a></li>
-                    <li className="mx-2"><a className="" href="https://www.linkedin.com/in/bhaumik-chauhan-415468257/"><FaLinkedin size={24}/></a></li>
-                    <li className="mx-2"><a className="" href="https://github.com/bhaumik0307"><FaGithub size = {24}/></a></li>
+                    <li className="mx-2"><a className="" target='_blank' href="https://www.instagram.com/bhaumik_chauhan_08/"><FaInstagram size={24}/></a></li>
+                    <li className="mx-2"><a className="" target='_blank' href="https://www.linkedin.com/in/bhaumik-chauhan-415468257/"><FaLinkedin size={24}/></a></li>
+                    <li className="mx-2"><a className="" target='_blank' href="https://github.com/bhaumik0307"><FaGithub size = {24}/></a></li>
                 </ul>
             </footer>
         </div>
