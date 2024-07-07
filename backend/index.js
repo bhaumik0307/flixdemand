37be995aeab2482d0683dadb3b3f6d087352f706
Bhaumik Chauhan <127184237+bhaumik0307@users.noreply.github.com>
2024-07-07 19:46:08 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,9 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('app is running')
+})
 app.use('/api/genres', genres)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
